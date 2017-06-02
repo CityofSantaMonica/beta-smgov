9fd59a59b7cc84721e47f967dc7c829b11e65297
sevilturner <sevil.turner@smgov.net>
2017-06-01 17:36:26 -0700

    moved push file

--- push.sh
+++ push.sh
@@ -1,7 +1,8 @@
+git config --local user.name "deploy"
+git config --local user.email deploy@smgov.net
 git checkout master
 git fetch
 git pull
 git add --all .
 git commit -m "Deploy content"
 git push origin master
-exit
